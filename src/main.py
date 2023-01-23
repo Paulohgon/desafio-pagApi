31df5657ec587314b92da4815ee9508bc42cc3ce
Paulo <paulo.henrique@jetbov.com>
2023-01-23 17:57:10 -0300

    .

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from routes.views import routingMap
 app = FastAPI()
 @app.get("/")
-async def root():
+def root():
   return {"Bem vindo a tranfeera api"}
 
 routingMap.create(app)
